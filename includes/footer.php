f1d46b586866c456a7652f4f57f0a1d8ee1aaede
Gadiraju Aravind <52728163+garavind2000@users.noreply.github.com>
2021-07-09 11:51:40 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  | Creater : <eum>Gadiraju Aravind </enum> |Contact Us: +91 6303485187</p>	
+        </center>
+    </div>
+</footer>
